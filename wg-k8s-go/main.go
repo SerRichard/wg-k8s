67cf55f2ef9836104e92d55a8135c1a7f48a9cab
sean <sean.hoyal@external.eodc.eu>
2025-11-21 15:58:05 +0100

    add basic cli

--- wg-k8s-go/main.go
+++ wg-k8s-go/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"os"
+	
+	"github.com/spf13/cobra"
+)
+
+func WireguardK8S() *cobra.Command {
+	command := &cobra.Command{
+		Use:   "wgk8s",
+		Short: "Wireguard K8s Toolkit.",
+		Long: `
+Command line tool for interacting with the Wireguard K8s toolkit. Currently, you can 
+use the wizard setup remote locations and output the config update for your other sites.`,
+		CompletionOptions: cobra.CompletionOptions{
+			DisableDefaultCmd: true,
+		},
+	}
+	// command.AddCommand(server.ServerCommand())
+	// command.AddCommand(daemon.DaemonCommand())
+
+	command.SetHelpCommand(&cobra.Command{
+		Hidden: true,
+	})
+
+	return command	
+}
 
 func main() {
-	fmt.Print("Package: wg_k8s_go!\n")
+
+	if err := WireguardK8S().Execute(); err != nil {
+		os.Exit(1)
+	}
+
 }
